806368228e71f7e3c4b085fd90b68be1839c1ab5
benitav <benita@journeyapps.com>
2025-04-02 10:45:59 +0200

    Use mintlify search in search bar, AI search as separate button

--- inkeep.js
+++ inkeep.js
@@ -22,19 +22,19 @@ const inkeepSettings = {
   };
    
   // The Mintlify search triggers, which we'll reuse to trigger the Inkeep modal
-  const searchButtonContainerIds = [
-    "search-bar-entry",
-    "search-bar-entry-mobile",
-  ];
+  // const searchButtonContainerIds = [
+  //   "search-bar-entry",
+  //   "search-bar-entry-mobile",
+  // ];
    
   // Clone and replace, needed to remove existing event listeners
-  const clonedSearchButtonContainers = searchButtonContainerIds.map((id) => {
-    const originalElement = document.getElementById(id);
-    const clonedElement = originalElement.cloneNode(true);
-    originalElement.parentNode.replaceChild(clonedElement, originalElement);
+  // const clonedSearchButtonContainers = searchButtonContainerIds.map((id) => {
+  //   const originalElement = document.getElementById(id);
+  //   const clonedElement = originalElement.cloneNode(true);
+  //   originalElement.parentNode.replaceChild(clonedElement, originalElement);
    
-    return clonedElement;
-  });
+  //   return clonedElement;
+  // });
    
   // Load the Inkeep component library
   const inkeepScript = document.createElement("script");
@@ -61,50 +61,50 @@ const inkeepSettings = {
     });
    
     // Instantiate the search bar modal
-    const inkeepSearchModal = Inkeep.ModalSearchAndChat({
-      baseSettings: {
-        ...inkeepSettings.baseSettings,
-        colorMode: {
-          sync: {
-            target: document.documentElement,
-            attributes: ["class"],
-            isDarkMode: (attributes) => !!attributes.class?.includes("dark"),
-          },
-        },
-      },
-      modalSettings: {
-        onOpenChange: handleOpenChange,
-      },
-    });
+    // const inkeepSearchModal = Inkeep.ModalSearchAndChat({
+    //   baseSettings: {
+    //     ...inkeepSettings.baseSettings,
+    //     colorMode: {
+    //       sync: {
+    //         target: document.documentElement,
+    //         attributes: ["class"],
+    //         isDarkMode: (attributes) => !!attributes.class?.includes("dark"),
+    //       },
+    //     },
+    //   },
+    //   modalSettings: {
+    //     onOpenChange: handleOpenChange,
+    //   },
+    // });
    
-    function handleOpenChange(newOpen) {
-      inkeepSearchModal.update({ modalSettings: { isOpen: newOpen } });
-    }
+    // function handleOpenChange(newOpen) {
+    //   inkeepSearchModal.update({ modalSettings: { isOpen: newOpen } });
+    // }
    
     // When the Mintlify search bar elements are clicked, open the Inkeep search modal
-    clonedSearchButtonContainers.forEach((trigger) => {
-      trigger.addEventListener("click", function () {
-        inkeepSearchModal.update({
-          modalSettings: { isOpen: true },
-        });
-      });
-    });
+    // clonedSearchButtonContainers.forEach((trigger) => {
+    //   trigger.addEventListener("click", function () {
+    //     inkeepSearchModal.update({
+    //       modalSettings: { isOpen: true },
+    //     });
+    //   });
+    // });
    
     // Open the Inkeep Modal with cmd+k
-    window.addEventListener(
-      "keydown",
-      (event) => {
-        if (
-          (event.metaKey || event.ctrlKey) &&
-          (event.key === "k" || event.key === "K")
-        ) {
-          event.stopPropagation();
-          inkeepSearchModal.update({
-            modalSettings: { isOpen: true },
-          });
-          return false;
-        }
-      },
-      true
-    );
+    // window.addEventListener(
+    //   "keydown",
+    //   (event) => {
+    //     if (
+    //       (event.metaKey || event.ctrlKey) &&
+    //       (event.key === "k" || event.key === "K")
+    //     ) {
+    //       event.stopPropagation();
+    //       inkeepSearchModal.update({
+    //         modalSettings: { isOpen: true },
+    //       });
+    //       return false;
+    //     }
+    //   },
+    //   true
+    // );
   });

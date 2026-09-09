4225cb9e0925a42b827edf4a727471916de700f4
marknesh <markmunene.nesh@gmail.com>
2026-09-09 10:52:08 +0300

    Move PowerSyncCardGroup to snippets folder

--- snippets/shared/powersync-card-group.jsx
+++ snippets/shared/powersync-card-group.jsx
@@ -1,4 +1,4 @@
-export function PowerSyncCardGroup({ children }) {
+export const PowerSyncCardGroup = ({ children }) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-x-4">
       {children}
